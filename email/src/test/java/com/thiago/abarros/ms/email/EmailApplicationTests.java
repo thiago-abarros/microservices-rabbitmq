0b241d2c02232d6b545eff12a6b81fef2e8e0971
Thiago Barros <bunbismuth@proton.me>
2024-09-06 18:58:44 -0300

    build: email microservice build

---
+++ email/src/test/java/com/thiago/abarros/ms/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.abarros.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
