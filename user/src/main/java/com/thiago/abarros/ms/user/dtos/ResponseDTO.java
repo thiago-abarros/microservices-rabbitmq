cfe239fa5220dc1ed03cc42bc518ec6d1ee632b6
Thiago Barros <bunbismuth@proton.me>
2024-09-08 19:30:48 -0300

    feat: added security config

---
+++ user/src/main/java/com/thiago/abarros/ms/user/dtos/ResponseDTO.java
@@ -0,0 +1,5 @@
+package com.thiago.abarros.ms.user.dtos;
+
+public record ResponseDTO(String name, String token) {
+
+}
--- user/src/main/java/com/thiago/abarros/ms/user/dtos/UserRecordDTO.java
+++ user/src/main/java/com/thiago/abarros/ms/user/dtos/UserRecordDTO.java
@@ -3,6 +3,7 @@ package com.thiago.abarros.ms.user.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserRecordDTO (@NotBlank String name, @NotBlank @Email String email, @NotBlank String password) {
+public record UserRecordDTO(@NotBlank String name, @NotBlank @Email String email,
+                            @NotBlank String password) {
 
 }
