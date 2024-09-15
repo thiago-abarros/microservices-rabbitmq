fc3d124bc55515b7c23482a7071405da5090dfbe
Thiago Barros <bunbismuth@proton.me>
2024-09-15 14:16:12 -0300

    feat: added new email for recovering password

--- user/src/main/java/com/thiago/abarros/ms/user/producers/UserProducer.java
+++ user/src/main/java/com/thiago/abarros/ms/user/producers/UserProducer.java
@@ -18,12 +18,22 @@ public class UserProducer {
   @Value(value = "${broker.queue.email.name}")
   private String routingKey;
 
-  public void publishMessageEmail(User user) {
+  public void publishRegisterMessageEmail(User user) {
     var emailDTO = new EmailDTO(
         user.getUserId(),
         user.getEmail(),
         "Cadastro de Usuário",
         "Seja bem-vindo(a) " + user.getName() + ", seu cadastro foi realizado com sucesso!"
+    );
+    rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+  }
+
+  public void publishRecoverPasswordMessageEmail(User user, String password) {
+    var emailDTO = new EmailDTO(
+        user.getUserId(),
+        user.getEmail(),
+        "Redefinir Senha",
+        "Sua nova senha é: " + password
     );
     rabbitTemplate.convertAndSend("", routingKey, emailDTO);
   }
