6a5e3eeed6d043e07ef1ddbe0d00cd8479d00ac5
Thiago Barros <bunbismuth@proton.me>
2024-09-15 14:12:50 -0300

    refactor: added forgot-password route to permit all list

--- user/src/main/java/com/thiago/abarros/ms/user/infra/security/SecurityConfig.java
+++ user/src/main/java/com/thiago/abarros/ms/user/infra/security/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+            .requestMatchers(HttpMethod.POST, "auth/forgot-password").permitAll()
             .anyRequest().authenticated()
         )
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
