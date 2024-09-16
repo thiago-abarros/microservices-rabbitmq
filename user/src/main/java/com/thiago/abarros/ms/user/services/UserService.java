3a5beadde64da92bb9026931610baf03b6981eb6
Thiago Barros <bunbismuth@proton.me>
2024-09-16 15:17:39 -0300

    feat: added scheduled password reset and changePassword logic

--- user/src/main/java/com/thiago/abarros/ms/user/services/UserService.java
+++ user/src/main/java/com/thiago/abarros/ms/user/services/UserService.java
@@ -1,5 +1,7 @@
 package com.thiago.abarros.ms.user.services;
 
+import com.rabbitmq.client.RpcClient.Response;
+import com.thiago.abarros.ms.user.dtos.ChangePasswordRequestDTO;
 import com.thiago.abarros.ms.user.dtos.LoginRequestDTO;
 import com.thiago.abarros.ms.user.dtos.RecoverRequestDTO;
 import com.thiago.abarros.ms.user.dtos.ResponseDTO;
@@ -10,6 +12,9 @@ import com.thiago.abarros.ms.user.producers.UserProducer;
 import com.thiago.abarros.ms.user.repository.UserRepository;
 import com.thiago.abarros.ms.user.utils.PasswordGenerator;
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,7 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
-
   final UserRepository userRepository;
   final UserProducer userProducer;
   final TokenService tokenService;
@@ -59,20 +63,49 @@ public class UserService {
   }
 
   @Transactional
-  public ResponseDTO forgotPassword(RecoverRequestDTO recoverRequestDTO) {
+  public void changePassword(ChangePasswordRequestDTO changePasswordRequest) {
+    User user = userRepository.findByEmail(changePasswordRequest.email())
+        .orElseThrow(() -> new RuntimeException("User not found"));
 
+    if (passwordEncoder.matches(changePasswordRequest.oldPassword(), user.getPassword())) {
+      user.setPassword(passwordEncoder.encode(changePasswordRequest.newPassword()));
+      this.userRepository.save(user);
+    } else {
+      throw new RuntimeException("Old password does not match");
+    }
+  }
+
+  @Transactional
+  public ResponseDTO forgotPassword(RecoverRequestDTO recoverRequestDTO) {
     User user = userRepository.findByEmail(recoverRequestDTO.email())
         .orElseThrow(() -> new RuntimeException("User not found"));
 
     if (user != null) {
+      var oldPassword = user.getPassword();
       var newPassword = PasswordGenerator.generateRandomPassword(14);
 
       user.setPassword(passwordEncoder.encode(newPassword));
-      userRepository.save(user);
+      this.userRepository.save(user);
       this.userProducer.publishRecoverPasswordMessageEmail(user, newPassword);
-
+      
+      this.schedulePasswordRevert(user, oldPassword, newPassword);      
+      
       return new ResponseDTO(user.getName(), newPassword);
     }
     return null;
+  }
+
+  private void schedulePasswordRevert(User user, String oldPassword, String newPassword) {
+    long expirationTimeMillis = 10; // 10 minutes expiration time
+
+    Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+        User currentUser = userRepository.findById(user.getUserId())
+            .orElseThrow(() -> new RuntimeException("User not found"));
+        
+        if (passwordEncoder.matches(newPassword, currentUser.getPassword())) {
+            currentUser.setPassword(oldPassword);
+            userRepository.save(currentUser);
+        }
+    }, expirationTimeMillis, TimeUnit.MINUTES);
   }
 }
