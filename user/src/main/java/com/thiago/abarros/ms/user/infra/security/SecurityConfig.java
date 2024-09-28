6d8c344d1c594ba017a90ed352d8bc66f3574bf9
Thiago Barros <bunbismuth@proton.me>
2024-09-28 11:10:16 -0300

    feat: added swagger-ui route to security config

--- user/src/main/java/com/thiago/abarros/ms/user/infra/security/SecurityConfig.java
+++ user/src/main/java/com/thiago/abarros/ms/user/infra/security/SecurityConfig.java
@@ -37,7 +37,9 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
             .requestMatchers(HttpMethod.POST, "auth/forgot-password").permitAll()
+            .requestMatchers("swagger-ui/**").permitAll()
             .anyRequest().authenticated()
+          
         )
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
