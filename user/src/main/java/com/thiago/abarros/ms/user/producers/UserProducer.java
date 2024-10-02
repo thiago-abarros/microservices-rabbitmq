fc809d026eca85368786e385156c128737b33589
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-10-02 20:44:17 -0300

    docs: added docs to UserProducer

--- user/src/main/java/com/thiago/abarros/ms/user/producers/UserProducer.java
+++ user/src/main/java/com/thiago/abarros/ms/user/producers/UserProducer.java
@@ -6,35 +6,54 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Produces messages for user registration and password recovery.
+ *
+ * @see #publishRegisterMessageEmail(User)
+ * @see #publishRecoverPasswordMessageEmail(User, String)
+ */
 @Component
 public class UserProducer {
 
-  final RabbitTemplate rabbitTemplate;
+    final RabbitTemplate rabbitTemplate;
+    /**
+     * The routing key for the email queue.
+     */
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
 
-  public UserProducer(RabbitTemplate rabbitTemplate) {
-    this.rabbitTemplate = rabbitTemplate;
-  }
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
-  @Value(value = "${broker.queue.email.name}")
-  private String routingKey;
+    /**
+     * Publishes a registration message to the email queue.
+     *
+     * @param user the user to send the registration message to
+     */
+    public void publishRegisterMessageEmail(User user) {
+        var emailDTO = new EmailDTO(
+                user.getUserId(),
+                user.getEmail(),
+                "Cadastro de Usuário",
+                "Seja bem-vindo(a) " + user.getName() + ", seu cadastro foi realizado com sucesso!"
+        );
+        rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+    }
 
-  public void publishRegisterMessageEmail(User user) {
-    var emailDTO = new EmailDTO(
-        user.getUserId(),
-        user.getEmail(),
-        "Cadastro de Usuário",
-        "Seja bem-vindo(a) " + user.getName() + ", seu cadastro foi realizado com sucesso!"
-    );
-    rabbitTemplate.convertAndSend("", routingKey, emailDTO);
-  }
-
-  public void publishRecoverPasswordMessageEmail(User user, String password) {
-    var emailDTO = new EmailDTO(
-        user.getUserId(),
-        user.getEmail(),
-        "Redefinir Senha",
-        "Sua nova senha é: " + password
-    );
-    rabbitTemplate.convertAndSend("", routingKey, emailDTO);
-  }
+    /**
+     * Publishes a password recovery message to the email queue.
+     *
+     * @param user     the user to send the password recovery message to
+     * @param password the new password
+     */
+    public void publishRecoverPasswordMessageEmail(User user, String password) {
+        var emailDTO = new EmailDTO(
+                user.getUserId(),
+                user.getEmail(),
+                "Redefinir Senha",
+                "Sua nova senha é: " + password
+        );
+        rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+    }
 }
