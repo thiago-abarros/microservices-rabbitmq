8bfbecb45c45a403ddcffbe1b4a629dc0947e226
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-10-02 20:44:38 -0300

    refactor: added lombok annotation to clean up code

--- user/src/main/java/com/thiago/abarros/ms/user/models/User.java
+++ user/src/main/java/com/thiago/abarros/ms/user/models/User.java
@@ -1,54 +1,21 @@
 package com.thiago.abarros.ms.user.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "TB_USERS")
 public class User implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID userId;
-  private String name;
-  private String email;
-  private String password;
-
-  public UUID getUserId() {
-    return userId;
-  }
-
-  public void setUserId(UUID userId) {
-    this.userId = userId;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    private String name;
+    private String email;
+    private String password;
 }
