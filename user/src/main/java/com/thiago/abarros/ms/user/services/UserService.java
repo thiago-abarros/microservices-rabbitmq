dc5bd5d5de821350eb6829b51bfde6ee3752defb
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-10-02 20:43:52 -0300

    refactor: extracted UserService interface

--- user/src/main/java/com/thiago/abarros/ms/user/services/UserService.java
+++ user/src/main/java/com/thiago/abarros/ms/user/services/UserService.java
@@ -1,106 +1,40 @@
 package com.thiago.abarros.ms.user.services;
 
-import com.thiago.abarros.ms.user.dtos.ChangePasswordRequestDTO;
-import com.thiago.abarros.ms.user.dtos.LoginRequestDTO;
-import com.thiago.abarros.ms.user.dtos.RecoverRequestDTO;
-import com.thiago.abarros.ms.user.dtos.ResponseDTO;
-import com.thiago.abarros.ms.user.dtos.UserRecordDTO;
-import com.thiago.abarros.ms.user.infra.security.TokenService;
+import com.thiago.abarros.ms.user.dtos.*;
 import com.thiago.abarros.ms.user.models.User;
-import com.thiago.abarros.ms.user.producers.UserProducer;
-import com.thiago.abarros.ms.user.repository.UserRepository;
-import com.thiago.abarros.ms.user.utils.PasswordGenerator;
 
-import lombok.RequiredArgsConstructor;
+public interface UserService {
 
-import java.util.Optional;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+    /**
+     * Registers a new user with the provided user record.
+     *
+     * @param userDTO the user record to register
+     * @return the registered user
+     * @throws RuntimeException if the user already exists
+     */
+    User registerUser(UserRecordDTO userDTO);
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+    /**
+     * Authenticates a user with the provided login request.
+     *
+     * @param loginRequestDTO the login request to authenticate
+     * @return a response containing the user's name and token if authentication is successful, or null otherwise
+     */
+    ResponseDTO loginUser(LoginRequestDTO loginRequestDTO);
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-  final UserRepository userRepository;
-  final UserProducer userProducer;
-  final TokenService tokenService;
-  final PasswordEncoder passwordEncoder;
+    /**
+     * Changes the password of a user with the provided change password request.
+     *
+     * @param changePasswordRequestDTO the change password request to process
+     * @throws RuntimeException if the old password does not match
+     */
+    void changePassword(ChangePasswordRequestDTO changePasswordRequestDTO);
 
-  @Transactional
-  public User registerUser(UserRecordDTO userDTO) {
-    Optional<User> user = this.userRepository.findByEmail(userDTO.email());
-    if (user.isEmpty()) {
-      User newUser = new User();
-      BeanUtils.copyProperties(userDTO, newUser);
-      newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
-
-      this.userRepository.save(newUser);
-      this.userProducer.publishRegisterMessageEmail(newUser);
-      return newUser;
-    }
-    return null;
-  }
-
-  public ResponseDTO loginUser(LoginRequestDTO loginRequest) {
-    User user = this.userRepository.findByEmail(loginRequest.email())
-        .orElseThrow(() -> new RuntimeException("User not found"));
-
-    if (passwordEncoder.matches(loginRequest.password(), user.getPassword())) {
-      var token = tokenService.generateToken(user);
-      return new ResponseDTO(user.getName(), token);
-    }
-
-    return null;
-  }
-
-  @Transactional
-  public void changePassword(ChangePasswordRequestDTO changePasswordRequest) {
-    User user = this.userRepository.findByEmail(changePasswordRequest.email())
-        .orElseThrow(() -> new RuntimeException("User not found"));
-
-    if (passwordEncoder.matches(changePasswordRequest.oldPassword(), user.getPassword())) {
-      user.setPassword(passwordEncoder.encode(changePasswordRequest.newPassword()));
-      this.userRepository.save(user);
-    } else {
-      throw new RuntimeException("Old password does not match");
-    }
-  }
-
-  @Transactional
-  public ResponseDTO forgotPassword(RecoverRequestDTO recoverRequestDTO) {
-    User user = this.userRepository.findByEmail(recoverRequestDTO.email())
-        .orElseThrow(() -> new RuntimeException("User not found"));
-
-    if (user != null) {
-      var oldPassword = user.getPassword();
-      var newPassword = PasswordGenerator.generateRandomPassword(14);
-
-      user.setPassword(passwordEncoder.encode(newPassword));
-      this.userRepository.save(user);
-      this.userProducer.publishRecoverPasswordMessageEmail(user, newPassword);
-      
-      this.schedulePasswordRevert(user, oldPassword, newPassword);      
-      
-      return new ResponseDTO(user.getName(), newPassword);
-    }
-    return null;
-  }
-
-  private void schedulePasswordRevert(User user, String oldPassword, String newPassword) {
-    long expirationTimeMillis = 10; // 10 minutes expiration time
-
-    Executors.newSingleThreadScheduledExecutor().schedule(() -> {
-        User currentUser = userRepository.findById(user.getUserId())
-            .orElseThrow(() -> new RuntimeException("User not found"));
-        
-        if (passwordEncoder.matches(newPassword, currentUser.getPassword())) {
-            currentUser.setPassword(oldPassword);
-            userRepository.save(currentUser);
-        }
-    }, expirationTimeMillis, TimeUnit.MINUTES);
-  }
+    /**
+     * Recovers the password of a user with the provided recover request.
+     *
+     * @param recoverRequestDTO the recover request to process
+     * @return a response containing the user's name and new password if recovery is successful, or null otherwise
+     */
+    ResponseDTO forgotPassword(RecoverRequestDTO recoverRequestDTO);
 }
