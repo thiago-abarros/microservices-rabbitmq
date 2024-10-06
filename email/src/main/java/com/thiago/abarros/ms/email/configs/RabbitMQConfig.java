6ab91e4ff81eff7057eec624dd70fb6c82e095a9
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2024-10-05 21:18:40 -0300

    feat: added two queues and max priority for them

--- email/src/main/java/com/thiago/abarros/ms/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/thiago/abarros/ms/email/configs/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.thiago.abarros.ms.email.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${broker.queue.email.name}")
-    private String queue;
+    @Value("${broker.queue.recover.password}")
+    private String queueRecoverPassword;
+
+    @Value("${broker.queue.register}")
+    private String queueEmailRegister;
 
     @Bean
-    public Queue queue() {
-        return new Queue(queue, true);
+    public Queue recoverPasswordQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-max-priority", 10);
+        return new Queue(queueRecoverPassword, true, false, false, args);
+    }
+
+    @Bean
+    public Queue emailRegisterQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-max-priority", 5);
+        return new Queue(queueEmailRegister, true, false, false, args);
     }
 
     @Bean
